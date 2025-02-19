45a60c3408b4650c8f3936591af993956369bdba
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-02-19 11:50:44 +0000

    Add sorting feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,66 @@
 import { useState, useEffect } from "react"
-import { Link, NavLink } from "react-router"
+import { Link } from "react-router"
 import { getArticles } from "../api/articles"
-
+import { getSortedArticles } from "../api/articles"
+import { useSearchParams } from "react-router"
+import SortDropdown from "../components/SortDropdown"
 const Home = () => {
+    const [searchParams] = useSearchParams()
+    const sortBy = searchParams.get('sort_by')
+    const order = searchParams.get('order')
     const [articles, setArticles] = useState([])
+    const [currentSort, setCurrentSort] = useState('Most recent')
+    const [showSortDropdown, setShowSortDropdown] = useState(false)
     useEffect(() => {
         getArticles()
             .then((articles) => {
                 setArticles(articles)
             })
     }, [])
+    useEffect(() => {
+        if (!sortBy && !order) return
+        const query = `sort_by=${sortBy}&order=${order}`
+        getSortedArticles(query)
+            .then((articles) => {
+                setArticles(articles)
+                setShowSortDropdown(false)
+                switch (query) {
+                    case "sort_by=created_at&order=desc":
+                        setCurrentSort('Most recent')
+                        break;
+                    case "sort_by=created_at&order=asc":
+                        setCurrentSort('Oldest first')
+                        break;
+                    case "sort_by=comment_count&order=desc":
+                        setCurrentSort('Most commented')
+                        break;
+                    case "sort_by=comment_count&order=asc":
+                        setCurrentSort('Less commented')
+                        break;
+                    case "sort_by=votes&order=desc":
+                        setCurrentSort('Most voted')
+                        break;
+                    case "sort_by=votes&order=asc":
+                        setCurrentSort('Less voted')
+                        break;
+                    default:
+                        setCurrentSort('Most recent')
+                }
+                return
+            })
+    }, [searchParams])
     return (
         <>
             <h1>Welcome to NC News!</h1>
+            <div className="controls">
+                <div className="group">
+                    <div className="label">Sort by:</div>
+                    <button onClick={() => {
+                        showSortDropdown ? setShowSortDropdown(false) : setShowSortDropdown(true)
+                    }}>{currentSort}</button>
+                    {showSortDropdown ? <SortDropdown /> : null}
+                </div>
+            </div>
             <div className="articles">
                 {articles.map((article) => {
                     return <Link to={`articles/${article.article_id}`} className="article" key={article.article_id}>
