5f1a3fc15fc5c5c664b143feeab06cf9e13b6e32
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-02-25 11:25:11 +0000

    Fix button for login

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -8,7 +8,7 @@ const MyProfile = ({ setUserData }) => {
                 <button
                     onClick={() => {
                         setUserData({ username: 'tickle122', name: "Tom Tickle", avatar_url: "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953" })
-                    }}>Log out</button>
+                    }}>Login</button>
             </div>
         )
     }
