4c48eb354951938872efb7d3eea4c7b2e6511c4c
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-03-16 13:31:38 +0000

    Update backend url after redeployemnt on a new account

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'https://nc-news-ih3j.onrender.com/api',
+    baseURL: 'https://nc-news-yuce.onrender.com',
     timeout: 5000
 });
 export default api
