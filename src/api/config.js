59053b101087041786e8b4e63f00bcd18cfa62fa
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-03-19 22:25:07 +0000

    Fix Axios instance to not send credentials

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const api = axios.create({
     baseURL: 'https://nc-news-yuce.onrender.com/api',
-    timeout: 5000,
-    withCredentials: true
+    timeout: 5000
 });
 export default api
